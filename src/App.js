e4699c11aa9ffde6ac86f9ad26cec4c186634d3c
unknown <saurab.rawat@jamsfy.com>
2022-12-08 18:21:29 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,27 +1,74 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Analytics from "analytics";
 import Perfume from "perfume.js";
-import perfumePlugin from "@analytics/perfumejs";
 import axios from "axios";
-console.log("perfume", Perfume);
 
-const analytics = Analytics({
-  app: "sample-react-app",
-  plugins: [
-    {
-      name: "test-plugin",
-      track: ({ payload }) => {
-        console.log(payload);
-        sendMessage(payload);
-      },
-    },
-    perfumePlugin({
-      perfume: Perfume,
-    }),
-  ],
+const perfume = new Perfume({
+  analyticsTracker: async (options) => {
+    const { attribution, metricName, data, navigatorInformation, rating } =
+      options;
+    switch (metricName) {
+      case "navigationTiming":
+        if (data && data.timeToFirstByte) {
+          await sendMessage("navigationTiming", data);
+          console.log(data);
+        }
+        break;
+      case "networkInformation":
+        if (data && data.effectiveType) {
+          await sendMessage("networkInformation", data);
+          console.log(data);
+        }
+        break;
+      case "storageEstimate":
+        await sendMessage("storageEstimate", data);
+        console.log(data);
+        break;
+      case "TTFB":
+        await sendMessage("timeToFirstByte", { duration: data });
+        console.log({ duration: data });
+        break;
+      case "RT":
+        await sendMessage("redirectTime", { duration: data });
+        console.log({ duration: data });
+        break;
+      case "FCP":
+        await sendMessage("firstContentfulPaint", { duration: data });
+        console.log({ duration: data });
+        break;
+      case "FID":
+        await sendMessage("firstInputDelay", { duration: data });
+        console.log({ duration: data });
+        break;
+      case "LCP":
+        await sendMessage("largestContentfulPaint", { duration: data });
+        console.log({ duration: data });
+        break;
+      case "CLS":
+        await sendMessage("cumulativeLayoutShift", { value: data });
+        console.log({ duration: data });
+        break;
+      case "INP":
+        await sendMessage("interactionToNextPaint", { value: data });
+        console.log({ duration: data });
+        break;
+      case "TBT":
+        await sendMessage("totalBlockingTime", { duration: data });
+        console.log({ duration: data });
+        break;
+      case "elPageTitle":
+        await sendMessage("elementTimingPageTitle", { duration: data });
+        console.log({ duration: data });
+        break;
+      default:
+        await sendMessage(metricName, { duration: data });
+        console.log({ duration: data });
+        break;
+    }
+  },
 });
 
+console.log(perfume)
 function App() {
   return (
     <div className="navbar">
@@ -57,7 +104,7 @@ function App() {
 }
 
 export default App;
-async function sendMessage(payload) {
+async function sendMessage(message, payload) {
   const headers = {
     "Content-type": "application/json",
     Authorization: `Bearer ${process.env.REACT_APP_HOSTEDHOOKS_API_KEY}`,
@@ -72,7 +119,7 @@ async function sendMessage(payload) {
             notes: "The event",
           },
           event_details: payload,
-          about_event: "this is result",
+          about_event: message,
         },
         version: "1.0",
         event_type: "metric.event",
